1d1d98f5fbc20a357c60cce0fba2a9982c7d0235
Breno Rodrigues <brenocartoon@hotmail.com>
2022-05-16 21:10:32 -0300

    Create new components

---
+++ src/components/Copyrights/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { styles } from './styles';
+
+export function Copyrights() {
+  return (
+    <View>
+      <Text style={styles.text}>
+        Feito com dedicação.
+      </Text>
+    </View>
+  );
+}
